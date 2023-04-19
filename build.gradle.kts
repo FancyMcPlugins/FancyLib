a3d4660d1115a01fed05fc175318062f0063d9d1
OliverSchlueter <oliverhd.1303@gmail.com>
2023-04-19 20:45:11 +0200

    Add jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "1.5.4"
+//    id("io.papermc.paperweight.userdev") version "1.5.4"
     id("maven-publish")
 }
 
@@ -12,8 +12,13 @@ java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
+repositories{
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
 dependencies {
-    paperweight.paperDevBundle("1.19.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 }
 
 tasks {
@@ -29,9 +34,9 @@ tasks {
     }
 
     // Configure reobfJar to run when invoking the build task
-    assemble {
-        dependsOn(reobfJar)
-    }
+//    assemble {
+//        dependsOn(reobfJar)
+//    }
 
     compileJava {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
