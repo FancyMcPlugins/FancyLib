7634814dfc102f2c779846fcc5184c4d091c796d
OliverSchlueter <oliverhd.1303@gmail.com>
2023-04-28 12:25:01 +0200

    Add ConfigHelper

---
+++ src/main/java/de/oliver/fancylib/ConfigHelper.java
@@ -0,0 +1,16 @@
+package de.oliver.fancylib;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class ConfigHelper {
+
+    public static Object getOrDefault(FileConfiguration config, String path, Object defaultVal){
+        if(!config.contains(path)){
+            config.set(path, defaultVal);
+            return defaultVal;
+        }
+
+        return config.get(path);
+    }
+
+}
