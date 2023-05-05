429f6ab662bd9407e33f90d6b036286ebd414de2
OliverSchlueter <oliverhd.1303@gmail.com>
2023-05-05 10:50:53 +0200

    Add FancyLib class

---
+++ src/main/java/de/oliver/fancylib/FancyLib.java
@@ -0,0 +1,9 @@
+package de.oliver.fancylib;
+
+import org.apache.maven.artifact.versioning.ComparableVersion;
+
+public class FancyLib {
+
+    public static final ComparableVersion VERSION = new ComparableVersion("1.0.0");
+
+}
