225ba14e0340d188823dd0d5a932f1fbe21cad10
OliverSchlueter <oliverhd.1303@gmail.com>
2023-05-13 15:25:01 +0200

    Add ServerSoftware.java

---
+++ src/main/java/de/oliver/fancylib/serverSoftware/ServerSoftware.java
@@ -0,0 +1,44 @@
+package de.oliver.fancylib.serverSoftware;
+
+import de.oliver.fancylib.serverSoftware.schedulers.FancyScheduler;
+import de.oliver.fancylib.serverSoftware.schedulers.BukkitScheduler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ServerSoftware {
+
+    public static boolean isFolia() {
+        try {
+            Class.forName("io.papermc.paper.threadedregions.scheduler.RegionScheduler");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static boolean isPaper() {
+        try {
+            Class.forName("io.papermc.paper.event.player.AsyncChatEvent");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static boolean isBukkit() {
+        try {
+            Class.forName("org.bukkit.Bukkit");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static FancyScheduler getCorrectScheduler(JavaPlugin plugin) {
+        if(isFolia()){
+            // return folia scheduler
+        }
+
+        return new BukkitScheduler(plugin);
+    }
+
+}
---
+++ src/main/java/de/oliver/fancylib/serverSoftware/schedulers/BukkitScheduler.java
@@ -0,0 +1,61 @@
+package de.oliver.fancylib.serverSoftware.schedulers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
+
+public class BukkitScheduler implements FancyScheduler {
+
+    BukkitTask bukkitTask;
+    JavaPlugin plugin;
+
+    public BukkitScheduler(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public @NotNull FancyScheduler runTask(Location location, Runnable task) {
+        bukkitTask = Bukkit.getScheduler().runTask(plugin, task);
+        return this;
+    }
+
+    @Override
+    public @NotNull FancyScheduler runTaskAsynchronously(Runnable task) {
+        bukkitTask = Bukkit.getScheduler().runTaskAsynchronously(plugin, task);
+        return this;
+    }
+
+    @Override
+    public @NotNull FancyScheduler runTaskLater(Location location, long delay, Runnable task) {
+        bukkitTask = Bukkit.getScheduler().runTaskLater(plugin, task, delay);
+        return this;
+    }
+
+    @Override
+    public @NotNull FancyScheduler runTaskLaterAsynchronously(long delay, Runnable task) {
+        bukkitTask = Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, task, delay);
+        return this;
+    }
+
+    @Override
+    public @NotNull FancyScheduler runTaskTimer(Location location, long delay, long period, Runnable task) {
+        bukkitTask = Bukkit.getScheduler().runTaskTimer(plugin, task, delay, period);
+        return this;
+    }
+
+    @Override
+    public @NotNull FancyScheduler runTaskTimerAsynchronously(long delay, long period, Runnable task) {
+        bukkitTask = Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, task, delay, period);
+        return this;
+    }
+
+    @Override
+    public void cancel() {
+        if (!bukkitTask.isCancelled()) {
+            bukkitTask.cancel();
+        }
+    }
+
+}
